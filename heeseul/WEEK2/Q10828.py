822caa355929417eb5c9ef95e9e5ea4e55fa1c67
YunHeeSeul <heeseul09@naver.com>
2023-01-13 15:44:51 +0900

    WEEK2 자료구조 문제풀이

---
+++ heeseul/WEEK2/Q10828.py
@@ -0,0 +1,24 @@
+import sys
+
+s = []
+for _ in range(int(sys.stdin.readline())):
+    order = sys.stdin.readline().split()
+    if order[0] == "push":
+        s.append(order[1])
+    elif order[0] == "pop":
+        if s:
+            print(s.pop())
+        else:
+            print(-1)
+    elif order[0] == "size":
+        print(len(s))
+    elif order[0] == "empty":
+        if s:
+            print(0)
+        else:
+            print(1)
+    elif order[0] == "top":
+        if s:
+            print(s[-1])
+        else:
+            print(-1)
