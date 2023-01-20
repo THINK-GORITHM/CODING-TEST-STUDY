bb83f66136e739c4bd98b4874a104b6fc86f983b
heehminh <mink141416@gmail.com>
2023-01-20 20:57:32 +0900

    baekjoon 18312 시각 - 스터디 추가문제

--- week3_bruteForce/b2309.py
+++ week3_bruteForce/b2309.py
@@ -1,11 +1,13 @@
 from itertools import combinations
 
-arr = []
-for _ in range(9):
-    arr.append(int(input()))
+# arr = []
+# for _ in range(9):
+#     arr.append(int(input()))
+
+arr = [int(input()) for _ in range(9)]
 
 for i in combinations(arr, 7):
     if sum(i) == 100:
         for j in  sorted(i):
             print(j)
-        break
+        break # 답이 여러가지 나올 수 있지만 1개만 출력해야함
