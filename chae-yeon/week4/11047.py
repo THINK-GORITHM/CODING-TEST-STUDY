bef2db0b89d3ad61e870f880deed239ec2860c56
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-02-14 19:41:14 +0900

    week7: Binary Search

--- chae-yeon/week4/11047.py
+++ chae-yeon/week4/11047.py
@@ -3,13 +3,11 @@ import sys
 # Ai는 Ai-1의 배수라는 말이 있으므로 그리디로 접근할 수 있다.
 
 n, k = map(int, sys.stdin.readline().split());
-m = [int(sys.stdin.readline())for _ in range(n)]
-m.reverse()
-total = 0
-for i in m:
-  if k//i==0: continue
-  else:
-    total+= k//i
-    k-=i*(k//i)
+coins = [int(sys.stdin.readline())for _ in range(n)]
+coins.reverse()
+ans = 0
+for coin in coins:
+  ans += k//coin
+  k%=coin
 
-print(total)
+print(ans)
